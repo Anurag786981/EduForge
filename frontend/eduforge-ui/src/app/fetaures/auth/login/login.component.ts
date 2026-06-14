33d5463759593cc9b7b02868e20ccb5895023157
Anurag <anuragtiwariji2012@gmail.com>
2026-06-14 20:32:25 +0530

    add login page with Angular Material and reactive form validation

--- frontend/eduforge-ui/src/app/fetaures/auth/login/login.component.ts
+++ frontend/eduforge-ui/src/app/fetaures/auth/login/login.component.ts
@@ -1,11 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+
+  hidePassword: boolean = true;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]]
+    });
+  }
+
+  onSubmit(): void {
+
+  console.log('Login button clicked');
+
+  console.log(this.loginForm.value);
+
+  console.log(this.loginForm.valid);
+
+  if (this.loginForm.invalid) {
+    console.log('Form is invalid');
+    this.loginForm.markAllAsTouched();
+    return;
+  }
+
+  console.log('Form is valid');
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.value);
+  }
 }
