c7fb3de6e71c27fdcd861bac3b344a037cb30280
Anurag <anuragtiwariji2012@gmail.com>
2026-06-14 11:35:35 +0530

    Initial Spring Boot setup With Swagger

---
+++ backend/eduforge-api/src/main/java/com/eduforge/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.eduforge.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.reflect.Executable;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
