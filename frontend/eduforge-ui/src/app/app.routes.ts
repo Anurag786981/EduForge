35d1a5d1cc243e036a5c5a22df6a5b9d43e11419
Gourav Mishra <gouravmishra90@gmail.com>
2026-06-23 00:31:00 +0530

    Adding partial basic layout of Teacher Dashboard page

--- frontend/eduforge-ui/src/app/app.routes.ts
+++ frontend/eduforge-ui/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
-import {TeacherSidebarComponent} from './features/teacher/teacher-dashboard/teacher-sidebar/teacher-sidebar.component';
-
+import {TeacherSidebarComponent} from './features/teacher/teacher-mainpage/teacher-sidebar/teacher-sidebar.component';
+import {TeacherDashboardComponent} from './features/teacher/teacher-mainpage/teacher-dashboard/teacher-dashboard.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'teacher-dashboard',
+    component: TeacherDashboardComponent
   },
   {
     path: 'teacher-sidebar',
